41532dddd939a30ace180648f613c199e1e2c33a
mateolohezic <mateolohezic@gmail.com>
2024-03-06 21:19:17 -0300

    New: auth

--- js/navbar.js
+++ js/navbar.js
@@ -2,3 +2,17 @@ function cerrarSesion(){
     localStorage.removeItem("logueado");
     window.location.href = "login.html";
 }
+
+function irAdministracion(){
+    window.location.href = "admin.html";
+}
+
+const sesion = localStorage.getItem("logueado");
+
+const botonSesion = document.getElementById("botonSesion");
+const administracionNavbar = document.getElementById("administracionNavbar");
+
+if(sesion){
+    botonSesion.innerHTML = '<button onclick="cerrarSesion()" class="nav-link text-white" >Cerrar sesión</button>'
+    administracionNavbar.innerHTML = '<a class="nav-link text-white" href="../pages/admin.html">Administración</a>'
+}
